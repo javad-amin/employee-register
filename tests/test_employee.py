ea8b0beda85a9e1d5d1e9b8bb5415b1afbd477ed
javad-amin <javad.amin@outlook.com>
2022-11-26 10:32:45 +0100

    add more test cases for missing employee attributes

--- tests/test_employee.py
+++ tests/test_employee.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 import pytest
 from pydantic import BaseModel
 
 
 class Employee(BaseModel):
     first_name: str
-    last_name: str
+    last_name: Optional[str] = ""
     email: str
 
 
@@ -12,3 +14,13 @@ def test_employee_empty() -> None:
     """We don't allow shallow employees"""
     with pytest.raises(Exception):
         Employee()
+
+def test_employee_missing_first_name() -> None:
+    """An employee without a first name is not acceptable"""
+    with pytest.raises(Exception):
+        Employee()
+
+def test_employee_missing_last_name() -> None:
+    """An employee without a last name is fine, let's not get so formal now"""
+    employee = Employee(first_name="Lady", email="lady@gaga.com")
+    assert employee.last_name == ""
