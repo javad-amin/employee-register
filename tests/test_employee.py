5444c6f7a5912262a849d8005e5e596623511485
javad-amin <javad.amin@outlook.com>
2022-11-26 10:34:45 +0100

    add test for missing employee email

--- tests/test_employee.py
+++ tests/test_employee.py
@@ -24,3 +24,8 @@ def test_employee_missing_last_name() -> None:
     """An employee without a last name is fine, let's not get so formal now"""
     employee = Employee(first_name="Lady", email="lady@gaga.com")
     assert employee.last_name == ""
+
+def test_employee_missing_email() -> None:
+    """An employee without a email address is not acceptable"""
+    with pytest.raises(Exception):
+        Employee(first_name="Lady", last_name="Gaga")
