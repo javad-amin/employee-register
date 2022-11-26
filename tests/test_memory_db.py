6e6a6d8a6562fd63101342cf2e1ba3b7b898c04e
javad-amin <javad.amin@outlook.com>
2022-11-26 11:13:33 +0100

    save employee object in memory

--- tests/test_memory_db.py
+++ tests/test_memory_db.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Final, Union
 
 
@@ -14,30 +14,45 @@ class DatabaseResponse:
 
 
 @dataclass
+class EmployeeItem:
+    identifier: str
+    email: str
+    first_name: str = ""
+    last_name: str = ""
+
+
+@dataclass
 class MemoryDB:
+    employees: list[EmployeeItem] = field(default_factory=list)
+
     def add_employee(
         self, first_name: str, last_name: str, email: str
     ) -> DatabaseResponse:
         new_employee = {
+            "identifier": "917c1ecd-4e07-4a1c-8f9e-f189e95564e6",
             "first_name": first_name,
             "last_name": last_name,
             "email": email,
         }
+
+        self.employees.append(EmployeeItem(**new_employee))
+
         return DatabaseResponse(content=new_employee)
 
 
 def test_add_employee() -> None:
-    memory_db = MemoryDB()
-    response = memory_db.add_employee(
-        first_name="James",
-        last_name="Bond",
-        email="james@bond.com",
-    )
-
-    assert isinstance(response, DatabaseResponse)
-    assert response.status == ResponseStatus.SUCCESS
-    assert response.content == {
+    employee = {
         "first_name": "James",
         "last_name": "Bond",
         "email": "james@bond.com",
     }
+    memory_db = MemoryDB()
+    response = memory_db.add_employee(**employee)
+
+    expected = employee.copy()
+    expected["identifier"] = "917c1ecd-4e07-4a1c-8f9e-f189e95564e6"
+
+    assert isinstance(response, DatabaseResponse)
+    assert response.status == ResponseStatus.SUCCESS
+    assert response.content == expected
+    assert memory_db.employees == [EmployeeItem(**expected)]
