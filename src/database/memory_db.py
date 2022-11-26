30432251173caa734501b313132ec1211b782635
javad-amin <javad.amin@outlook.com>
2022-11-26 11:56:43 +0100

    add more tests

--- src/database/memory_db.py
+++ src/database/memory_db.py
@@ -2,7 +2,7 @@ import uuid
 from dataclasses import dataclass, field
 
 from database.item import EmployeeItem
-from database.response import DatabaseResponse
+from database.response import DatabaseResponse, ResponseStatus
 
 
 @dataclass
@@ -12,6 +12,12 @@ class MemoryDB:
     def add_employee(
         self, first_name: str, last_name: str, email: str
     ) -> DatabaseResponse:
+        if self._email_already_registered(email):
+            return DatabaseResponse(
+                "Employee with the given email address is already registered.",
+                ResponseStatus.FAILURE,
+            )
+
         new_employee = {
             "identifier": str(uuid.uuid4()),
             "first_name": first_name,
@@ -26,3 +32,21 @@ class MemoryDB:
     def get_employees(self) -> DatabaseResponse:
         employees_response = [employee.__dict__ for employee in self.employees]
         return DatabaseResponse(content=employees_response)
+
+    def remove_employee(self, identifier: str) -> DatabaseResponse:
+        for i, employee in enumerate(self.employees):
+            if identifier == employee.identifier:
+                self.employees.pop(i)
+                return DatabaseResponse(
+                    {"message": "Employee was removed successfully"}
+                )
+        return DatabaseResponse(
+            f"The employee with employee identifier {identifier} not found.",
+            ResponseStatus.FAILURE,
+        )
+
+    def _email_already_registered(self, email) -> bool:
+        for employee in self.employees:
+            if email.lower() == employee.email.lower():
+                return True
+        return False
--- tests/test_memory_db.py
+++ tests/test_memory_db.py
@@ -46,3 +46,36 @@ def test_get_employees(pre_populated_database: MemoryDB) -> None:
         email="lady@queen.com",
     )
     assert len(pre_populated_database.get_employees().content) == 5
+
+
+def test_remove_employee(pre_populated_database: MemoryDB) -> None:
+    second_employee = pre_populated_database.employees[1]
+    uuid_to_delete = second_employee.identifier
+
+    pre_populated_database.remove_employee(identifier=uuid_to_delete)
+
+    # As the pre_populated_database has four employees after deleting we should have three left
+    assert len(pre_populated_database.employees) == 3
+
+    assert second_employee not in pre_populated_database.employees
+
+
+def test_remove_employee_no_match_found(pre_populated_database: MemoryDB) -> None:
+    none_existing_id = "06335e84-2872-4914-8c5d-3ed07d2a2f16"
+    response = pre_populated_database.remove_employee(none_existing_id)
+    assert response.status == ResponseStatus.FAILURE
+
+
+def test_add_employee_duplicate_email(pre_populated_database: MemoryDB) -> None:
+    pre_populated_database.add_employee(
+        first_name="Lady",
+        last_name="Gaga",
+        email="lady@queen.com",
+    )
+
+    response = pre_populated_database.add_employee(
+        first_name="Mr",
+        last_name="Gaga",
+        email="lady@queen.com",
+    )
+    assert response.status == ResponseStatus.FAILURE
