f60c57cceb73f36848d79d5ab67d0a24bbaf79e6
javad-amin <javad.amin@outlook.com>
2022-11-26 11:02:58 +0100

    add content to tje Database response

--- tests/test_memory_db.py
+++ tests/test_memory_db.py
@@ -1,18 +1,43 @@
 from dataclasses import dataclass
+from typing import Final, Union
 
 
+class ResponseStatus:
+    FAILURE: Final = "failure"
+    SUCCESS: Final = "success"
+
+
+@dataclass
 class DatabaseResponse:
-    pass
+    status: str = ResponseStatus.SUCCESS
+    content: dict | str = ""
 
 
 @dataclass
 class MemoryDB:
-    def add_employee(self) -> DatabaseResponse:
-        return DatabaseResponse()
+    def add_employee(
+        self, first_name: str, last_name: str, email: str
+    ) -> DatabaseResponse:
+        new_employee = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+        }
+        return DatabaseResponse(content=new_employee)
 
 
 def test_add_employee() -> None:
     memory_db = MemoryDB()
-    response = memory_db.add_employee()
+    response = memory_db.add_employee(
+        first_name="James",
+        last_name="Bond",
+        email="james@bond.com",
+    )
 
     assert isinstance(response, DatabaseResponse)
+    assert response.status == ResponseStatus.SUCCESS
+    assert response.content == {
+        "first_name": "James",
+        "last_name": "Bond",
+        "email": "james@bond.com",
+    }
